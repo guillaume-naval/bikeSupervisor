617bd9e6467272e37f885b82b25c963b40c117f3
Guillaume NAVAL <navalguillaume@gmail.com>
2022-01-22 10:52:04 +0100

    Project init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import * as VueGoogleMaps from "vue2-google-maps";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyCJdU7Q9J3U1Ger63PApwgtEZRv-KxyoOU",
+    libraries: "places",
+  },
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  vuetify,
+  render: (h) => h(App),
+}).$mount("#app");
